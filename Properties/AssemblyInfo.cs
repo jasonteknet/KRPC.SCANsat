9d1bd4b7bd93b5b0b8a880c938b47841ac81a913
Jason Higgins <jason@Jasons-MacBook-Pro.local>
2026-02-11 19:48:23 -0600

    Align assembly version with v1.1.0

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -13,5 +13,5 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("a8ce1e64-e7ad-41f4-9eb0-b4c5e1b92242")]
 
-[assembly: AssemblyVersion("0.1.0")]
-[assembly: AssemblyFileVersion("0.1.0")]
+[assembly: AssemblyVersion("1.1.0")]
+[assembly: AssemblyFileVersion("1.1.0")]
